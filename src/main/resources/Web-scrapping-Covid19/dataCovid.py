d3f5b616b760159ccb719e1d3966ad29b85a65e5
María Lourdes <37163754+narommm@users.noreply.github.com>
2022-06-14 22:01:54 -0600

    Scrolling test

--- src/main/resources/Web-scrapping-Covid19/dataCovid.py
+++ src/main/resources/Web-scrapping-Covid19/dataCovid.py
@@ -6,30 +6,30 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
-
 driver = webdriver.Chrome('D:\chromedriver.exe')
 driver.get('https://covid19.gob.sv/')
 
-last_height = driver.execute_script('return c')
+last_height = driver.execute_script('return document.body.scrollHeight')
 
 while True:
     driver.execute_script('window.scrollTo(0, document.body.scrollHeight)')
-    time.sleep(2)
+    time.sleep(5)
     new_height = driver.execute_script('return document.body.scrollHeight')
     if new_height == last_height:
         break
     last_height = new_height
     
 soup = BeautifulSoup(driver.page_source, 'lxml')
+
 charts_covid = soup.find_all('div', class_ = 'igc-graph-group')
 
-df = pd.DataFrame({'Link':['']})
+df = pd.DataFrame({'Chart':['']})
 
 for chart in charts_covid:
     try:
-        link = chart.find('rect', class_ = 'igc-crosshair-overlay').get('href')
-        df = df.append({'Link':link}, ingnore_index = True)
+        chart = chart.find('rect', class_ = 'igc-crosshair-overlay').get('href')
+        df = df.append({'Chart':chart}, ingnore_index = True)
     except:
         pass
-    break
-
+    
+df.to_csv('D:\covid.csv')
